d8253552894b9b60c84b5458b078f4ab32bdf3c4
Lubava <Lwm28@rambler.ru>
2023-12-14 23:09:22 +0300

    изменено

---
+++ src/main/java/Task/TaskManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package Task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TaskManagementSystemApplication!
+ *
+ */
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementSystemApplication.class,
+                args);
+    }
+}
